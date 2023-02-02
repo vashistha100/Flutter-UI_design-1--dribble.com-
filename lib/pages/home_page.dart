a1637d368f132507f84ad0f95afafe623afa2fae
Manish Sharma <manishsharma5016@gmail.com>
2023-02-02 12:49:28 +0530

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,325 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  double availableScreenWidth = 0;
+  int selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    availableScreenWidth = MediaQuery.of(context).size.width - 50;
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 25),
+            alignment: Alignment.bottomCenter,
+            height: 170,
+            decoration: BoxDecoration(color: Colors.blue[800]),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: const [
+                    Text(
+                      "Riotters",
+                      style: TextStyle(
+                          fontSize: 26,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "Team Folder",
+                      style: TextStyle(fontSize: 17, color: Colors.white),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: Colors.black.withOpacity(.1)),
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.search,
+                          size: 28,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: Colors.black.withOpacity(.1)),
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.notifications,
+                          size: 28,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                RichText(
+                  text: const TextSpan(
+                      text: "Storage",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold),
+                      children: [
+                        TextSpan(
+                          text: " 9.1/10GB",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w300,
+                            fontSize: 16,
+                          ),
+                        )
+                      ]),
+                ),
+                const Text(
+                  "Upgrade",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: Row(
+              children: [
+                buildFileSizeChart("SOURCES", Colors.blue, .3),
+                const SizedBox(
+                  width: 2,
+                ),
+                buildFileSizeChart("DOCS", Colors.red, .25),
+                const SizedBox(
+                  width: 2,
+                ),
+                buildFileSizeChart("IMAGES", Colors.yellow, .20),
+                const SizedBox(
+                  width: 2,
+                ),
+                buildFileSizeChart("", Colors.grey.shade200, .23),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          const Divider(
+            height: 20,
+          ),
+          Expanded(
+            child: ListView(
+              padding: const EdgeInsets.all(25),
+              children: [
+                const Text(
+                  "Recently Added",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                Row(
+                  children: [
+                    buildFileColumn('diamond', 'desktop', '.sketch'),
+                    SizedBox(
+                      width: availableScreenWidth * 0.03,
+                    ),
+                    buildFileColumn('diamond', 'mobile', '.sketch'),
+                    SizedBox(
+                      width: availableScreenWidth * 0.03,
+                    ),
+                    buildFileColumn('diamond', 'interaction', '.sketch'),
+                  ],
+                ),
+                const Divider(
+                  height: 60,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Text(
+                      "Projects",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      'Create new',
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                buildProjectRow('Chatbox'),
+                buildProjectRow('TimeNote'),
+                buildProjectRow('Chatbox'),
+                buildProjectRow('Chatbox'),
+                buildProjectRow('Chatbox'),
+              ],
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: Container(
+        decoration: const BoxDecoration(shape: BoxShape.circle, boxShadow: [
+          BoxShadow(color: Colors.white, spreadRadius: 7, blurRadius: 1)
+        ]),
+        child: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.add),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: (index) {
+            setState(() {
+              selectedIndex = index;
+            });
+          },
+          currentIndex: selectedIndex,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.access_time), label: 'Time'),
+            BottomNavigationBarItem(icon: Icon(Icons.add_box), label: 'Folder'),
+          ]),
+    );
+  }
+
+  Container buildProjectRow(String foldername) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 8),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      height: 65,
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              const Icon(
+                Icons.folder,
+                color: Colors.blue,
+              ),
+              const SizedBox(
+                width: 12,
+              ),
+              Text(
+                foldername,
+                style:
+                    const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+              ),
+            ],
+          ),
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.more_vert_rounded,
+                color: Colors.grey,
+              ))
+        ],
+      ),
+    );
+  }
+
+  Column buildFileColumn(String image, String filename, String extension) {
+    return Column(
+      children: [
+        Container(
+          width: availableScreenWidth * .31,
+          decoration: BoxDecoration(
+              color: Colors.grey[200], borderRadius: BorderRadius.circular(20)),
+          padding: const EdgeInsets.all(38),
+          height: 110,
+          child: Image.asset("assets/images/$image.png"),
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+        RichText(
+          text: TextSpan(
+            text: filename,
+            style: const TextStyle(
+                color: Colors.black, fontSize: 14, fontWeight: FontWeight.w600),
+            children: [
+              TextSpan(
+                text: extension,
+                style: const TextStyle(
+                    color: Colors.grey,
+                    fontSize: 12,
+                    fontWeight: FontWeight.w300),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+  Column buildFileSizeChart(String title, Color color, double widthPercentage) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          height: 4,
+          width: availableScreenWidth * widthPercentage,
+          color: color,
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        Text(
+          title,
+          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 10),
+        ),
+      ],
+    );
+  }
+}
